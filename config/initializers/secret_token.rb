076e27c8e3820624ef79565ba4d5bed3d7deba8f
RichardWigley <RichardWigley@gmail.com>
2012-07-03 18:47:05 +0100

    Inititial check in
    Created new project
     + postgresql
     + database yml changed user
     + capybara
     + spork
     + guard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScheduleGardener::Application.config.secret_token = '04fdc051def49298391b91a5450241c4a697e707d7bd9e6861b28e8cd1dd2811509e903f856b0dd4722a78e15221447efed6ed80b27862dba3c132f12596c85e'
