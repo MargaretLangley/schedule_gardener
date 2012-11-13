e922b0b448511692f51548903b4d836f6b738377
Richard Wigley <richard.wigley@gmail.com>
2012-11-13 17:09:47 +0000

    calendar - moved appointment code into helpers. To see what I've got

--- app/helpers/appointment_helper.rb
+++ app/helpers/appointment_helper.rb
@@ -6,4 +6,17 @@ module AppointmentHelper
     appointment.appointee.full_name
   end
 
+  def appointment_has_slot_booked?(appointments,slot)
+    appointments &&
+    appointments.any? { |appointment| appointment.include_slot_number?(slot) }
+  end
+
+  def find_appointment_for_slot(appointments,slot)
+    appointments.find { |appointment| appointment.include_slot_number?(slot) }
+  end
+
+  def new_appointment_for_date_and_slot_link(date,slot)
+    link_to "#{slot}", new_appointment_path(starts_at: Appointment.starts_at_from_date_and_slot(date,slot)), class: 'btn'
+  end
+
 end
