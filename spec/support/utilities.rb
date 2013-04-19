fc2d543294a8a7e950c12436f8287124f464d13a
Richard Wigley <richard.wigley@gmail.com>
2013-04-19 18:08:42 +0100

    visit  is a capybara method which are used in functional/integration tests. Cookies are only avaiable in rspec requests tests. rspec 2 is making a clear distinction between functional and requests and so I made the change.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,7 +11,5 @@ def visit_signin_and_login(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
-  # Sign in when not using Capybara as well.
-  cookies[:remember_token] = user.remember_token
 end
 
