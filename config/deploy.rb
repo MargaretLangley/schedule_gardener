e6c9d3c2c107703286942c3f51a984ef4f6f9f23
Richard Wigley <richard.wigley@gmail.com>
2013-04-21 21:11:12 +0100

    capistrano recipe to configure the app on vps

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,92 +1,35 @@
-set :application,     "schedule_gardener"
-set :dump_options,    "-Fc --no-acl --no-owner"
-set :restore_options, "--verbose --clean --no-acl --no-owner"
+require "bundler/capistrano"
+require "capistrano-rbenv"
 
-task :stpush   do
-  puts "#{application} code to staging"
-  run_locally 'git push staging master'
-end
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/postgresql"
+load "config/recipes/check"
+load "config/recipes/monit"
 
-task :stmig do
-  run_locally 'heroku run rake db:migrate --remote staging'
-  run_locally 'heroku ps --remote staging'
-end
+set  :host, "193.183.99.251"
+server "#{host}", :web, :app, :db, primary: true
+# set :server_name, "draca.co.uk www.draca.co.uk"
+set :server_name, "bcs.io www.bcs.io"
 
-task :stinfo  do
-  puts "#{application} pg information"
-  system 'heroku pg:info --remote staging'
-end
+set :user, "deployer"
+set :application, "schedule_gardener"  #change
+set :owner, "BCS-io" #change
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-task :stlogs do
- system 'heroku logs -t --remote staging'
-end
+set :scm, "git"
+set :repository, "git@github.com:#{owner}/#{application}.git"
+set :branch, "master"
+set :rbenv_ruby_version, "1.9.3-p392"
 
-task :stpsql do
-  puts "#{application} Remote Staging Database"
-  system 'heroku pg:psql HEROKU_POSTGRESQL_PURPLE --remote staging'
-end
+set :maintenance_template_path, File.expand_path("../recipes/templates/maintenance.html.erb", __FILE__)
 
-task :strestore do
-  start_banner("wiping out and resoring the STAGING Database")
-  run_locally "pg_dump #{dump_options} #{application}_development > tmp/schedule_gardener.dump"
-  run_locally "pg_restore -W #{restore_options} -h ec2-54-243-235-169.compute-1.amazonaws.com -U alawohniburtoq -d dcgonjhl76emj8 -p 5432 tmp/schedule_gardener.dump"
-  run_locally 'rm tmp/schedule_gardener.dump'
-end
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+# ssh_options[:keys] = [File.join(ENV["HOME"], ".vagrant.d", "insecure_rivate_key")]
 
-def start_banner (title)
-  puts "\n\n" + "*" * 80
-  puts "*" + " " * 78 + "*"
-  puts "*" * 10 + " #{application} #{title} " + "*" * 10
-  puts "*" + " " * 78 + "*"
-  puts "*" * 80 + "\n\n"
-end
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
-task :pdpush do
-  start_banner("updating the PRODUCTION Web server")
-  system 'git push production master'
-end
-
-task :pdinfo  do
-  system 'heroku pg:info --remote production'
-end
-
-task :pdlogs do
- system 'heroku logs -t --remote production'
-end
-
-task :pdpsql do
-  start_banner("Remote PRODUCTION Database")
-  system 'heroku pg:psql HEROKU_POSTGRESQL_MAROON --remote production'
-end
-
-
-task :pdrestore do
-  start_banner("wiping out and resoring the PRODUCTION Database")
-  run_locally "pg_dump #{dump_options} #{application}_development > data.dump"
-  run_locally "pg_restore -W #{restore_options} -h ec2-54-243-190-152.compute-1.amazonaws.com -U cuzufrejeteocm -d d1re16pjjfhcp7 -p 5432 data.dump"
-  run_locally 'rm data.dump'
-end
-
-
-task :pdbackup do
-  start_banner("Backing up PRODUCTION Database, drops and restores local")
-  puts "backing up local database"
-  run_locally "pg_dump #{dump_options} #{application}_development > tmp/#{application}_backup.dump"
-  puts "#{application} Capturing...."
-  system 'heroku pgbackups:capture --expire --app gardener-production'
-  puts "#{application} Copied to local...."
-  system 'curl -o tmp/latest.dump `heroku pgbackups:url --app gardener-production`'
-  puts "drop local database and create"
-  system "rake db:drop"
-  system "rake db:create"
-  puts "#{application} replacing the local database...."
-  system "pg_restore #{restore_options} -d #{application}_development tmp/latest.dump"
-
-  puts "prepare test db"
-  system 'rake db:test:prepare'
-end
-
-# More ideas while getting hang of scripts ... must remove comments!
-#https://devcenter.heroku.com/articles/multiple-environments
-#https://github.com/capistrano/capistrano/wiki/2.x-Getting-Started
-#https://gist.github.com/2577832
