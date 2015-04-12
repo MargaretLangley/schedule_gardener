528504af6be2a8257b14d089ca1f24ac375e554d
Richard Wigley <richard.wigley@gmail.com>
2015-04-12 14:44:37 +0100

    DateTimeWrapper - using attr_reader to instead of raw variable Refactoring and moving method to private Timezone related bug

--- lib/date_time_wrapper.rb
+++ lib/date_time_wrapper.rb
@@ -2,26 +2,39 @@
 # DateTimeWrapper
 #
 class DateTimeWrapper
-  attr_accessor :datetime
+  attr_reader :datetime
   def persisted?
     false
   end
 
   def initialize(args)
     @datetime =  args.fetch(:datetime, nil)
-    @datetime = date_and_time_to_datetime(args) if @datetime.nil?
+    @datetime = to_datetime(args) if datetime.nil?
   end
 
   def date
-    @datetime.to_date
+    datetime.to_date
   end
 
   def time
-    @datetime.strftime '%H:%M'
+    datetime.strftime '%H:%M'
   end
 
-  def date_and_time_to_datetime(args)
+  private
+
+  #
+  # Bug
+  # DateTime.strptime(date_time, '%d %b %Y %H:%M').in_time_zone
+  # Requires you to to explicitly include time zone which don't always have.
+  # Safe to use Time.zone.parse
+  #
+  # Example Parse "1 Sep 2012 08:00:00"
+  #
+  # DateTime.strptime => Sat, 01 Sep 2012 09:00:00 BST +01:00
+  # Time.zone.parse   => Sat, 01 Sep 2012 08:00:00 BST +01:00
+  #
+  def to_datetime(args)
     date_time = "#{args.fetch(:date)} #{args.fetch(:time)}:00"
-    DateTime.strptime(date_time, '%d %b %Y %H:%M')
+    Time.zone.parse(date_time)
   end
 end
--- spec/date_time_wrapper_spec.rb
+++ spec/date_time_wrapper_spec.rb
@@ -5,25 +5,24 @@ describe DateTimeWrapper do
   before { Timecop.freeze(Time.zone.parse('1/9/2012 8:00')) }
 
   context 'Initialize' do
-    context 'with datetime' do
+    it 'handles datetime' do
+      expect(DateTimeWrapper.new(datetime: Time.zone.now).datetime)
+        .to eq Time.zone.local(2012, 9, 1, 8, 0)
+    end
+
+    it 'handles date and time' do
+      args = { date: '1 Sep 2012', time: '08:00' }
+      expect(DateTimeWrapper.new(args).datetime)
+        .to eq Time.zone.local(2012, 9, 1, 8, 0)
+    end
+
+    describe 'accessors' do
       it 'date' do
         expect(DateTimeWrapper.new(datetime: Time.zone.now).date.to_s).to eq '2012-09-01'
       end
 
       it 'time' do
         expect(DateTimeWrapper.new(datetime: Time.zone.now).time.to_s).to eq '08:00'
-      end
-    end
-
-    context 'with date and time' do
-      it 'date' do
-        args = { date: '1 Sep 2012', time: '08:00' }
-        expect(DateTimeWrapper.new(args).date.to_s).to eq '2012-09-01'
-      end
-
-      it 'time' do
-        args = { date: '1 Sep 2012', time: '08:00' }
-        expect(DateTimeWrapper.new(args).time.to_s).to eq '08:00'
       end
     end
   end
