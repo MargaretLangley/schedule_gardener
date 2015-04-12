2274ad7d4bb34c3a4cf94f33762c6f665f2c4024
Richard Wigley <richard.wigley@gmail.com>
2015-04-12 13:15:41 +0100

    Routes to explicit verbs

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 ScheduleGardener::Application.routes.draw do
   root to: 'static_pages#home'
-  match '/help',                  to: 'static_pages#help'
-  match '/about',                 to: 'static_pages#about'
-  match '/contact',               to: 'static_pages#contact'
-  match '/password_reset_sent',   to: 'static_pages#password_reset_sent'
+  get '/help',                  to: 'static_pages#help'
+  get '/about',                 to: 'static_pages#about'
+  get '/contact',               to: 'static_pages#contact'
+  get '/password_reset_sent',   to: 'static_pages#password_reset_sent'
 
-  match '/signup',                to: 'users#new',    via: :get
-  match '/signup',                to: 'users#create', via: :post
-  match '/settings/profile/:id',  to: 'users#edit',   via: :get, as: :edit_profile
-  match '/settings/profile/:id',  to: 'users#update', via: :put, as: :update_profile
+  get '/signup',                to: 'users#new'
+  post '/signup',                to: 'users#create'
+  get '/settings/profile/:id',  to: 'users#edit',  as: :edit_profile
+  put '/settings/profile/:id',  to: 'users#update', as: :update_profile
   resources :users, only: [:index, :destroy]
 
-  match '/signin',                to: 'sessions#new',     via: :get
-  match '/signin',                to: 'sessions#create',  via: :post
+  get '/signin',                to: 'sessions#new'
+  post '/signin',                to: 'sessions#create'
   match '/signout',               to: 'sessions#destroy', via: :delete
 
   resources :dashboard, only: [:show]
