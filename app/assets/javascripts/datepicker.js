c63de2db8eafa90208764ad54db00b945304a131
Richard Wigley <richard.wigley@gmail.com>
2015-04-19 13:35:35 +0100

    BUGFIX Turbolink error

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -3,7 +3,7 @@
 // method configures datepicker
 // https://github.com/Nerian/bootstrap-datepicker-rails
 //
-$(function() {
+$(document).on("page:load ready", function(){
   $('input.date_picker').datepicker({
     format: 'dd/mm/yyyy',
     autoclose: true
