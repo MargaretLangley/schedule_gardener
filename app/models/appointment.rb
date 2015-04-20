ff0f15b9dbd3c31b9b34f1153fdcee94ac7e9886
Richard Wigley <richard.wigley@gmail.com>
2015-04-20 11:12:09 +0100

    Appointment removed association that was not required   - comment updated

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -22,17 +22,11 @@ class Appointment < ActiveRecord::Base
   belongs_to :contact, class_name: 'Contact', foreign_key: 'contact_id'
   belongs_to :appointee, class_name: 'Contact', foreign_key: 'appointee_id'
 
-  #
-  # TODO: appointment_time is a method to convert from start_at, end_at to
-  #       DateAndTimeRange
-  #
-  has_one :appointment_time
-  accepts_nested_attributes_for :appointment_time
-
   validates :appointee, :contact, :starts_at, presence: true
   validates_datetime :starts_at, after: :now, before: :this_date_next_year
   validates_datetime :ends_at, on_or_after: :starts_at
 
+  # TODO: change date time validation to use only 1 datetime gem
   # This code could replace the above and then remove validates_timeliness
   # validates :starts_at, date: { after: proc { Time.zone.now },
   #                               before: proc { Time.zone.now + 1.year } }
