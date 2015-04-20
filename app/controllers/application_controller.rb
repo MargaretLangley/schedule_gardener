974afb59a320ba526d112e99cd8090c9ae3bda8c
Richard Wigley <richard.wigley@gmail.com>
2015-04-20 12:33:50 +0100

    application_controller refactored and documented current_user related methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,36 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :signed_in?, :current_user?, :current_gardener?
   include StorePath
 
+  # saves current user
+  #  - assumes we have authenticated
+  #
   def sign_in_remember_session(user)
     # remember_token is a unique identifier
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
 
+  # forgets current user
+  #
   def sign_out_forget_session
     self.current_user = nil
     cookies.delete(:remember_token)
   end
 
+  # Get user from user database
+  #
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  attr_writer :current_user
+
+  def signed_in?
+    current_user.present?
+  end
+
+  def current_user?(user)
+    user == current_user
   end
 
   def current_admin?
@@ -28,22 +45,14 @@ class ApplicationController < ActionController::Base
     current_user && current_user.role == 'gardener'
   end
 
-  attr_writer :current_user
-
-  #  only people who are signed in can access users pages
+  # Authenticated users continue using application
+  #   - unauthenticated users are ask to authenticate
+  #
   def guest_redirect_to_signin_path
     return if signed_in?
 
     store_path
     redirect_to signin_path, notice: 'Please sign in.'
-  end
-
-  def signed_in?
-    current_user.present?
-  end
-
-  def current_user?(user)
-    user == current_user
   end
 
   rescue_from CanCan::AccessDenied do |exception|
