c482edce1e579a585c8889b605b81dfe394d6a1c
Richard Wigley <richard.wigley@gmail.com>
2015-04-24 14:05:59 +0100

    Administrate link added for an admin role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :signed_in?, :current_user?, :current_gardener?
+  helper_method :current_user, :signed_in?, :current_admin?, :current_user?, :current_gardener?
   include StorePath
 
   # saves current user
